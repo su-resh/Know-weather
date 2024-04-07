573a9c3e5f14d5ff491a6ab16d0b054c3423a568
Suresh Subedi <sureshsubedi3994@gmail.com>
2024-04-08 00:40:04 +0545

    2nd part made

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -7,28 +12,149 @@ class WeatherScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: const Text(
-            'Weather App',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
+        appBar: AppBar(
+            title: const Text(
+              'Weather App',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          centerTitle: true,
-          actions: [
-           IconButton(
-             icon: const Icon(Icons.refresh),
-             onPressed: (){},
-           ),
-          ]),
-          body: const Column(
+            centerTitle: true,
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.refresh),
+                onPressed: () {},
+              ),
+            ]),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             // main card
-           children: [
-            Placeholder(fallbackHeight: 250,
-            child: Text("Main Card"),
-            ),
-           ],
-          )
-    );
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Card(
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                      child: const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              "300°F",
+                              style: TextStyle(
+                                  fontSize: 32, fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Icon(
+                              Icons.cloud,
+                              size: 50,
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Rain",
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              // Weather forecast cards
+              const Text(
+                "Weather Forecast",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      child: HourlyForecastItem()
+                      ),
+                      HourlyForecastItem(),
+                      HourlyForecastItem(),
+                       HourlyForecastItem(), 
+                      HourlyForecastItem(),
+                      HourlyForecastItem(),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Placeholder(
+                fallbackHeight: 150,
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+
+class HourlyForecastItem extends StatelessWidget {
+  const HourlyForecastItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+                        elevation: 6,
+                        child: Container(
+                          width: 100,
+                          padding: const EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            children: [
+                              Text(
+                                "03:00",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Icon(
+                                Icons.cloud,
+                                size: 30,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "320.12",
+                                style: TextStyle(
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
   }
 }
